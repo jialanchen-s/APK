8af8d32602aebce2359fe8eb70aa4f78824599af
jialanchen-s <jialanchen66@163.com>
2026-09-13 01:44:27 +0800

    chore: 删除 source_package 目录（原始平台副本，已不参与构建）

--- source_package/server/modules/role-manager/role-manager.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RoleManagerController } from './role-manager.controller';
-
-@Module({
-  controllers: [RoleManagerController],
-})
-export class RoleManagerModule {}
